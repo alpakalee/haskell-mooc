c38db7759e0b6a08cc64d73a60c1491f6c237e88
tjwns1400 <tjwns1400@gmail.com>
2023-12-01 11:33:02 +0900

    Set3a_ex4

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -79,9 +79,13 @@ mapMaybe2 _ _ _               = Nothing
 palindromeHalfs :: [String] -> [String]
 palindromeHalfs xs = map firstHalf (filter palindrome xs)
 
-firstHalf = todo
+firstHalf str = take halfLen str
+  where
+    len = length str
+    halfLen = if even len then len `div` 2 else (len `div` 2) + 1
 
-palindrome = todo
+palindrome :: String -> Bool
+palindrome str = str == reverse str
 
 ------------------------------------------------------------------------------
 -- Ex 5: Implement a function capitalize that takes in a string and
